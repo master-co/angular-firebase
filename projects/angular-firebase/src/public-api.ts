d5a0c859ebad97f5591ad72d3781fc7706c2f968
Aron <aron886tw@gmail.com>
2021-03-23 17:35:28 +0800

    fix: export issue

--- projects/angular-firebase/src/public-api.ts
+++ projects/angular-firebase/src/public-api.ts
@@ -2,16 +2,16 @@
  * Public API Surface of angular-firebase
  */
 
-export * from './lib/analytics';
-export * from './lib/auth';
-export * from './lib/database';
-export * from './lib/firestore';
-export * from './lib/functions';
-export * from './lib/installations';
-export * from './lib/messaging';
-export * from './lib/performance';
-export * from './lib/remote-config';
-export * from './lib/storage';
+export * from './lib/analytics/index';
+export * from './lib/auth/index';
+export * from './lib/database/index';
+export * from './lib/firestore/index';
+export * from './lib/functions/index';
+export * from './lib/installations/index';
+export * from './lib/messaging/index';
+export * from './lib/performance/index';
+export * from './lib/remote-config/index';
+export * from './lib/storage/index';
 
 export * from './lib/firebase.module';
 export * from './lib/firebase.service';
