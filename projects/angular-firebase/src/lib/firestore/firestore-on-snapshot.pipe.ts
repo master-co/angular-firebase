1167c6c37d486910118024485c05d4d40e14d119
BenSeage <bg012300@gmail.com>
2021-03-24 17:47:25 +0800

    fix: firestore-on-snapshot

--- projects/angular-firebase/src/lib/firestore/firestore-on-snapshot.pipe.ts
+++ projects/angular-firebase/src/lib/firestore/firestore-on-snapshot.pipe.ts
@@ -1,36 +1,59 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import firebase from 'firebase/app';
+
 @Pipe({
     name: 'firestoreOnSnapshot'
 })
 export class FirestoreOnSnapshotPipe implements PipeTransform {
 
-    items = [];
+    result;
     observer;
 
     async transform(collectionRef): Promise<any[]> {
-        this.items = [];
-        if (!collectionRef) {
-            return this.items;
-        }
+        if (!collectionRef)
+            return this.result;
+
         if (this.observer) {
             this.observer();
         }
         await new Promise((resolve: Function) => {
             this.observer = collectionRef
-                .onSnapshot((snapshot) => {
-                    this.items.length = 0;
-                    snapshot.forEach((eachDoc) => {
-                        const item: any = {
-                            id: eachDoc.id,
-                            ...eachDoc.data()
-                        };
-                        this.items.push(item);
-                    });
+                .onSnapshot((snapshot: firebase.firestore.DocumentSnapshot | firebase.firestore.QuerySnapshot) => {
+                    if (snapshot instanceof firebase.firestore.QuerySnapshot) {
+                        if (this.result) {
+                            this.result.length = 0;
+                        } else {
+                            this.result = [];
+                        }
+
+                        snapshot.docs.forEach(eachDoc => {
+                            const item: any = {
+                                id: eachDoc.id,
+                                ...eachDoc.data()
+                            };
+                            this.result.push(item);
+                        });
+                    } else {
+                        if (this.result) {
+                            for (const key in this.result) {
+                                delete this.result[key];
+                            }
+                        } else {
+                            this.result = {};
+                        }
+
+                        const data = snapshot.data();
+                        for (const key in data) {
+                            this.result[key] = data[key];
+                        }
+                        this.result = snapshot.id;
+                    }
+
                     resolve();
-                })
+                });
         });
-        return this.items;
+        return this.result;
     }
 
     ngOnDestroy(): void {
